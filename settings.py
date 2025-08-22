3e77a58541ecfc933fe1725ee92f9b9d89663309
Cahoii <tangth023@gmail.com>
2025-08-22 22:59:48 +0700

    creating apps

--- settings.py
+++ settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "Tclass.apps.TclassConfig",
+    "Ttodo.apps.TtodoConfig",
 ]
 
 MIDDLEWARE = [
